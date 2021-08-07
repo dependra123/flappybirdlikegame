0dd7ac2cbb88ebc6a2d643de56f3728b624a5cc8
dependra123 <dependra.khatri12@gmail.com>
2021-08-07 16:49:25 -0600

    jefff

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ def main():
         if keys[py.K_SPACE] and p.y - player_vel + ship.get_height() / 2 > 0:
             p.y -= player_vel 
         else:
-            dt = clock.tick(FPS) * 0.30
+            dt = clock.tick(FPS) /2*1.5
             if p.y + ship.get_height() < HEIGHT :
                 vel = 2 * dt
                 p.y += vel
@@ -74,9 +74,34 @@ def main():
 
 
 
+def main_menu():
+    FPS = 60
+    clock = py.time.Clock()
+    title_font = py.font.SysFont("comicsans", 70, bold=False, italic=False)
+    start_font = py.font.SysFont("comicsans",50)
+    start_in = 3
+    run = True
+    while run:
+        clock.tick(FPS)
+        WIN.blit(BG, (-50, -80)) 
+        title_label = title_font.render("Press the mouse to begin...", 1, (255,255,255))
+        WIN.blit(title_label, (WIDTH/2 - title_label.get_width()/2, HEIGHT/2))
+        py.display.update()
+        for event in py.event.get():
+            if event.type == py.QUIT:
+                run = False
+            if event.type == py.MOUSEBUTTONDOWN:
+                while start_in > 0:
+                    
+                    start_label = start_font.render(str(start_in), 1, (255,255,255))
+                    WIN.blit(start_label, (WIDTH/2 - start_label.get_width()/2, HEIGHT/2))
+                    start_in -=1
+                    continue
 
+                main()
     py.quit()
 
 
+
 if __name__ == "__main__":
-    main()
+    main_menu()
